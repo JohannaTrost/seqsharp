84cf00bed0235ade14f1defd7899eec3fc067320
Johanna Trost <johanna.trost@univ-lyon1.fr>
2021-03-23 15:13:08 +0100

    update: k-fold cross validation,  weight decay

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,7 +4,10 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os
 from data_preprocessing import alignments_from_fastas, build_dataset
+from sklearn.model_selection import KFold
+from datetime import datetime
 
 
 def accuracy(outputs, labels):
@@ -53,8 +56,8 @@ def evaluate(model, val_loader):
 
 def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
     history = []
-    optimizer = opt_func(model.parameters(), lr)
-    for epoch in range(epochs):
+    optimizer = opt_func(model.parameters(), lr, weight_decay=1)
+    for epoch in range(0, epochs):
         # training Phase
         for batch in train_loader:
             loss = model.training_step(batch)
@@ -67,60 +70,93 @@ def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
         history.append(result)
     return history
 
-
 def main(args):
     try:
         fasta_dir = args[0]
-        fig_dir = None
+        model_dir = None
         if len(args) > 1:
-            fig_dir = args[1]
+            model_dir = args[1]
     except IndexError:
-        print('At least one argument is required: path to the directory containing the fasta files.')
-
+        print('At least 1 argument is required: path to the directory containing the fasta files\nOptional second '
+              'argument: path to the directory where results will be stored')
 
     batch_size = 128
     nb_seqs_per_align = 50
     nb_classes = 40  # number of multiple alignments
-    epochs = 30
+    epochs = 15
     lr = 0.001
+    nb_folds = 5
+
+    torch.manual_seed(42)
+
+    # k-fold validator
+    kfold = KFold(nb_folds, shuffle=True)
 
     # preprocessing of data
     raw_alignments = alignments_from_fastas(fasta_dir, nb_seqs_per_align, nb_classes)
     min_seq_len = min(len(min([seq for seqs in raw_alignments for seq in seqs], key=len)), 200)
 
-    train_alignments, val_alignments = np.split(np.asarray(raw_alignments), [int(nb_seqs_per_align*0.9)], axis=1)
+    # create unique subdir for the models
+    timestamp = datetime.now().strftime("%d-%b-%Y-%H:%M:%S.%f")
+    if model_dir is not None:
+        model_dir = model_dir + '/models-' + str(timestamp)
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
 
-    train_ds = build_dataset(train_alignments, min_seq_len)
-    val_ds = build_dataset(val_alignments, min_seq_len)
+    train_history = []
+    fold_eval = []
+    for fold, (train_ids, val_ids) in enumerate(kfold.split(raw_alignments[0])): # [0] because the seqs within one class shall be split not the classes
+        # train_alignments, val_alignments = np.split(np.asarray(raw_alignments), [int(nb_seqs_per_align*0.9)], axis=1)
+        print(f'FOLD {fold}')
+        print('----------------------------------------------------------------')
 
-    model_input_size = train_ds.data.shape[1] * train_ds.data.shape[2]
-    # val_ds, train_ds = random_split(ds, [round(len(ds) * 0.1), len(ds) - round(len(ds) * 0.1)])
+        # splitting dataset by splitting within each class
+        train_ds = build_dataset(np.take(raw_alignments, train_ids, axis=1), min_seq_len)
+        val_ds = build_dataset(np.take(raw_alignments, val_ids, axis=1), min_seq_len)
+        # val_ds, train_ds = random_split(ds, [round(len(ds) * 0.1), len(ds) - round(len(ds) * 0.1)])
 
-    train_loader = DataLoader(train_ds, batch_size, shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size)
+        train_loader = DataLoader(train_ds, batch_size, shuffle=True)
+        val_loader = DataLoader(val_ds, batch_size)
 
-    # generate model
-    model = Model(model_input_size, nb_classes)
+        # generate model
+        model_input_size = train_ds.data.shape[1] * train_ds.data.shape[2]
+        model = Model(model_input_size, nb_classes)
 
-    # train and validate model
-    history = fit(epochs, lr, model, train_loader, val_loader)
+        # train and validate model
+        train_history.append(fit(epochs, lr, model, train_loader, val_loader))
+        fold_eval.append(train_history[fold][-1]['val_acc'])
 
-    # plot the model evaluation
-    accuracies = [result['val_acc'] for result in history]
-    losses = [result['val_loss'] for result in history]
-    fig, axs = plt.subplots(2, 1, constrained_layout=True)
-    axs[0].plot(accuracies, '-x')
-    axs[0].set_xlabel('epoch')
-    axs[0].set_ylabel('accuracy')
-    axs[0].set_title('Accuracy vs. No. of epochs')
+        # Saving the model
+        print('Training process has finished. Saving trained model.\n')
+        if model_dir is not None:
+            torch.save(model.state_dict(), f'{model_dir}/model-fold-{fold}.pth')
 
-    axs[1].plot(losses, '-x')
-    axs[1].set_xlabel('epoch')
-    axs[1].set_ylabel('loss')
-    axs[1].set_title('Loss vs. No. of epochs')
 
-    if fig_dir is not None:
-        plt.savefig(fig_dir + '/fig.png')
+    # Print fold results
+    print(f'K-FOLD CROSS VALIDATION RESULTS FOR {nb_folds} FOLDS')
+    print('----------------------------------------------------------------')
+    for i, acc in enumerate(fold_eval):
+        print(f'Fold {(i+1)}: {acc} %')
+    print(f'Average: {np.sum(fold_eval) / len(fold_eval)} %')
+
+    # plot the model evaluation per fold
+    fig, axs = plt.subplots(1*nb_folds, 2, constrained_layout=True)
+    for i, fold_history in enumerate(train_history):
+        accuracies = [result['val_acc'] for result in fold_history]
+        losses = [result['val_loss'] for result in fold_history]
+
+        axs[i][0].plot(accuracies, '-x')
+        axs[i][0].set_xlabel('epoch')
+        axs[i][0].set_ylabel('accuracy')
+        axs[i][0].set_title(f'Fold {(i+1)}: Accuracy vs. No. of epochs')
+
+        axs[i][1].plot(losses, '-x')
+        axs[i][1].set_xlabel('epoch')
+        axs[i][1].set_ylabel('loss')
+        axs[i][1].set_title(f'Fold {i+1}: Loss vs. No. of epochs')
+
+    if model_dir is not None:
+        plt.savefig(model_dir + '/fig.png')
 
 
 if __name__ == '__main__':
