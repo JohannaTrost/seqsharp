Big fix: ignore batch that does not contain class when averaging over batch-class-accuracies

ConvNet.py
@@ -263,7 +263,7 @@ def accuracy(outputs, labels):
         class_mask = (labels == label).clone().detach()
         n = torch.sum(class_mask)
         n_correct = torch.sum(preds[class_mask] == labels[class_mask]).item()
-        accs[key] = n_correct / n if n_correct > 0 else torch.tensor(0.0)
+        accs[key] = n_correct / n if n > 0 else torch.tensor(-1)
         accs[key].to(compute_device)
 
     # acc = torch.tensor((torch.sum(preds == labels).item() / len(preds)))

train_eval.py
@@ -32,8 +32,10 @@ def evaluate(model, val_loader):
         losses.append(batch_loss)
 
         accs.append(batch_acc['acc'])
-        accs_emp.append(batch_acc['acc_emp'])
-        accs_sim.append(batch_acc['acc_sim'])
+        if batch_acc['acc_emp'] != -1:  # if == -1 no emp. data in this batch
+            accs_emp.append(batch_acc['acc_emp'])
+        if batch_acc['acc_sim'] != -1:  # if == -1 no sim. data in this batch
+            accs_sim.append(batch_acc['acc_sim'])
     # combine losses and accuracies
     epoch_loss = torch.stack(losses).mean()
     epoch_acc = torch.stack(accs).mean()