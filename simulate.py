bb40b0cfeec16b3d3d5a8ad68c69ac184dedf8fa
johanna.trost <johanna.trost@univ-lyon1.fr>
2022-11-17 15:13:41 +0100

    Add option to simulate with heterogeneous gamma rates

--- simulate.py
+++ simulate.py
@@ -7,29 +7,32 @@ from utils import n_sites_pdf, gamma_shape_pdf
 
 sub_m = 'Poisson'
 n_prof = '0256'
+gamma = 'G8'
 mix_m_def = f'../../EDCluster/Distributions/hogenom' \
             f'/udm_hogenom_{n_prof}_lclr_iqtree.nex'
 mix_m = f'UDM{n_prof}LCLR'
 
 tree_path = '../../data/hogenom_trees'
-out_path = f'../../data/simulations/alisim_{sub_m.lower()}_{mix_m.lower()}'
-if not os.path.exists(out_path):
-    os.mkdir(out_path)
+out_path = f'../../data/simulations/alisim_{sub_m.lower()}_{mix_m.lower()}' \
+           f'_{gamma.lower()}'
+os.mkdir(out_path)
 
 trees = [t for t in os.listdir(tree_path) if t.endswith('.tree')]
 n_sims = len(trees)
 seq_lens = n_sites_pdf.draw(n_sims)
+gamma_shapes = gamma_shape_pdf.draw(n_sims)
 
 n_sims_succ = 0
 files_sim_fail = []
 for i in tqdm(range(n_sims)):
     out_fasta = trees[i].split('coretree_')[1].split('.')[0]
     seq_len = n_sites_pdf.draw()[0]
+    gamma_opt = f'+{gamma}{"{"}{gamma_shapes[i]}{"}"}' if gamma != '' else gamma
     bash_cmd = (
         f'iqtree2 --alisim {out_path}/{out_fasta} '
         f'-t {tree_path}/{trees[i]} '
         f'-mdef {mix_m_def} '
-        f'-m {sub_m}+{mix_m} -mwopt -af fasta --seqtype AA '
+        f'-m {sub_m}+{mix_m}{gamma_opt} -mwopt -af fasta --seqtype AA '
         f'--length {int(seq_lens[i])}')
 
     process = subprocess.Popen(bash_cmd, shell=True,
